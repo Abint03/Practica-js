25f8ed94af99d63d2889d3ac8f4020ddf2e3eddf
Abigail Alegre <alegreav@gmail.com>
2023-10-19 00:21:27 -0300

    Update XMLHttpRequest.js

--- XMLHttpRequest.js
+++ XMLHttpRequest.js
@@ -3,14 +3,14 @@
 let datosJson; 
 let xhr= new XMLHttpRequest(); //se utiliza en programacion ajax 
 
-xhr.open('GET',"persona.json",true);
+xhr.open('GET',"https://rickandmortyapi.com/api/character/10",true);
 
 xhr.responseType='json';
 xhr.onload=function(){
     if(xhr.status===200){
         datosJson=xhr.response
         let texto= document.getElementById("nombre");
-        texto.textContent=datosJson.nombre;
+        texto.textContent=datosJson.name;
     }else{
         console.log("error");
     }
